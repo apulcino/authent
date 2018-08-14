212ab082bf71563097a189b92c64a5c6bc8d3c34
pulcino <alfredo.pulcino@afp.com>
2018-08-14 15:41:21 +0200

    faire addMembership avec toutes les adresse publique

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ server.listen(port, function () {
 });
 
 const regMgr = new regsitryMgr();
-const mcRecver = new multicastRecver(constantes.getServerIpAddress(), constantes.MCastAppPort, constantes.MCastAppAddr, (address, port, message) => {
+const mcRecver = new multicastRecver(constantes.getServerIpAddress(), constantes.MCastAppPort, constantes.MCastAppAddr, constantes.getServerPublicIpAddress(), (address, port, message) => {
   traceMgr.debug('Recv Msg From : ' + address + ':' + port + ' - ' + JSON.stringify(message));
   if (message.type === constantes.MSMessageTypeEnum.regAnnonce) {
     regMgr.add(message.host, message.port);
